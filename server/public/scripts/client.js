3ef79ea2c616c7923ed560c0d7ae4fd471501c2f
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-10-16 22:10:50 -0500

    Added GET ROUTE, GET Function, and AppendFunction. TESTED

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,34 @@ console.log('Hello From JS');
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('Hello from Jquery!')
+    console.log('Hello from Jquery!');
+    getTasks();
 }
+
+// Call GET Route Server side, and appends all info rows from the Database to the DOM
+function getTasks() {
+    $.ajax({
+        type: 'GET',
+        url: '/todo'
+    }).then((response) => {
+        console.log('GET Response', response);
+        appendTasks(response);
+    }).catch((error) => {
+        console.log('GET Error', error);
+    });
+} // End of getTasks
+
+// Appends information from Database to DOM || Must be used in tandem with GET request||
+function appendTasks(array) {
+    $('.outputField').empty();
+    console.log('DataValues', array);
+    for (let i = 0; i < array.length; i++) {
+        $(".outputField").append(`
+        <li data-id=${array[i].id}>
+        <p> ${array[i].task}: ${array[i].status} </p>
+        <button class="completeBtn">Complete Task</button>
+        <button class="deleteBtn">DELETE</button>
+        </li>
+        `);
+    }
+} // End of appendTasks
--- server/routes/todo_router.js
+++ server/routes/todo_router.js
@@ -3,4 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
+// GET route, will grab all rows in a data base and arrange them by their ID
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "tasks" ORDER BY "id";';
+    pool.query(queryText).then((result) => {
+        res.send( result.rows );
+    }).catch((error)=>{
+        console.log("error with get request",error);
+        res.sendStatus(500);
+    });
+});
+
 module.exports = router;
